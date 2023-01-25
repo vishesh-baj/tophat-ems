cda475ae3d2f2998781e45bc413ccfecd984171e
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-01-25 15:09:13 +0530

    added redux store and added a counter slice as an example for the specfiic functionality feature that we expect to be implemented in redux

--- client/src/slices/counter/CounterSlice.ts
+++ client/src/slices/counter/CounterSlice.ts
@@ -1,14 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-
 export interface CounterState {
   value: number;
 }
-
 const initialState: CounterState = {
   value: 0,
 };
-
 export const counterSlice = createSlice({
   name: "counter",
   initialState,
--- client/src/store.ts
+++ client/src/store.ts
@@ -1,21 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    // counter: counterReducer,
+  },
 });
-
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
-
-const downloadMultiple = (urls) => {
-  urls.forEach(function (url) {
-    var link = document.createElement("a");
-    link.href = url;
-    link.download = url.substring(url.lastIndexOf("/") + 1);
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  });
-};
