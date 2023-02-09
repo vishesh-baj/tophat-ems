dd99fea93800fc27d2be19e28517e61824fd5188
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-02-09 14:51:07 +0530

    added changes to candidate table

--- server/controllers/candidateController.js
+++ server/controllers/candidateController.js
@@ -1,5 +1,18 @@
 const Candidates = require("../schemas/Candidate");
 
+const getAllCandidates = async (req, res) => {
+  try {
+    const candidatesList = await Candidates.find({});
+    if (candidatesList.length === 0)
+      res.status(204).json({ message: "No candidates found", candidatesList });
+    res
+      .status(200)
+      .json({ message: "fetched candidates successfully.", candidatesList });
+  } catch (error) {
+    res.status(400).json({ message: "An error occured", error: error.message });
+  }
+};
+
 const addCandidate = async (req, res) => {
   try {
     const candidatePayload = req.body;
@@ -19,12 +32,39 @@ const addCandidate = async (req, res) => {
   }
 };
 
-const editCandidate = async (req,res) => {
-    
+const editCandidate = async (req, res) => {
+  try {
+    const candidateId = req.params.id;
+    const editedPayload = req.body;
+    const candidateExists = await Candidates.findById(candidateId);
+    if (!candidateExists)
+      return res.status(400).json({ message: "Candidate doesnt exist" });
+    const candidateToBeChanged = await Candidates.findByIdAndUpdate(
+      candidateId,
+      editedPayload
+    );
+    const changedCandidate = await candidateToBeChanged.save();
+    res
+      .status(200)
+      .json({ message: "Candidate changed successfully", changedCandidate });
+  } catch (error) {
+    res.status(400).json({ message: "Error occured", error: error.message });
+  }
+};
+
+const deleteCandidate = async (req, res) => {
+  const candidateId = req.params.id;
+  const candidateToBeDeleted = await Candidates.findByIdAndDelete(candidateId);
+  if (!candidateToBeDeleted)
+    res.status(400).json({ message: "Candidate doesnot exist" });
+  res.status(200).json({ message: "candidate deleted", candidateToBeDeleted });
 };
 
 module.exports = {
   candidateController: {
+    getAllCandidates,
     addCandidate,
+    editCandidate,
+    deleteCandidate,
   },
 };
